546955572c263ecf7bbcfce9d46df197ecc28e12
woo-jk <201702042@o.cnu.ac.kr>
2022-12-15 23:26:41 +0900

    feat: App 기능 선택 커맨드 입력 과정 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+const Validator = require("./Validator");
+
 class App {
   play() {}
+
+  requestFeatureCommand() {
+    InputView.readFeatureSelection((command) => {
+      this.handleError(() => {
+        Validator.validateFeatureCommand(command);
+        if (command === "1") this.fairMatching();
+      }, this.requestFeatureCommand.bind(this));
+    });
+  }
+
+  fairMatching() {}
+
+  handleError(callback, request) {
+    try {
+      callback();
+    } catch (error) {
+      OutputView.printErrorMessage(error);
+      request();
+    }
+  }
 }
 
 module.exports = App;
