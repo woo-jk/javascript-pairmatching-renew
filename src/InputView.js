3f7f3d65af858f7a02c3c423f9571e37b357bd8c
woo-jk <201702042@o.cnu.ac.kr>
2022-12-15 23:15:23 +0900

    feat: InputView 기능 선택 입력 함수, 미션 선택 입력 함수, 재매칭 입력 함수 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,17 @@
-const InputView = {};
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const InputView = {
+  readFeatureSelection(callback) {
+    MissionUtils.Console.readLine("기능을 선택하세요.\n1. 페어 매칭\n2. 페어 조회\n3. 페어 초기화\nQ. 종료", callback);
+  },
+
+  readMissionSelection(callback) {
+    MissionUtils.Console.readLine("과정, 레벨, 미션을 선택하세요.\nex) 백엔드, 레벨1, 자동차경주", callback);
+  },
+
+  readRematchingCommand(callback) {
+    MissionUtils.Console.readLine("매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n네 | 아니오", callback);
+  },
+};
 
 module.exports = InputView;
