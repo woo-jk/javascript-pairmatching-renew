955ee1587e5e03ea0caf767f3828772866ac1642
woo-jk <201702042@o.cnu.ac.kr>
2022-12-15 23:06:25 +0900

    feat: OutputView 과정, 미션 출력 함수, 페어 매칭 결과 출력 함수 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,26 @@
-const OutputView = {};
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const OutputView = {
+  MissionOverView: [
+    "\n#############################################",
+    "과정: 백엔드 | 프론트엔드",
+    "미션:",
+    "  - 레벨1: 자동차경주 | 로또 | 숫자야구게임",
+    "  - 레벨2: 장바구니 | 결제 | 지하철노선도",
+    "  - 레벨3:",
+    "  - 레벨4: 성능개선 | 배포",
+    "  - 레벨5:",
+    "#############################################",
+  ],
+
+  printMissionOverView() {
+    this.MissionOverView.forEach((text) => MissionUtils.Console.print(text));
+  },
+
+  printPairMatchingList(pairMatchingList) {
+    MissionUtils.Console.print("\n페어 매칭 결과입니다.");
+    pairMatchingList.forEach((pair) => MissionUtils.Console.print(pair));
+  },
+};
 
 module.exports = OutputView;
