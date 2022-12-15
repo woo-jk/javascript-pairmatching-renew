cafe16594b36c27ced12b669b11828ac8299987b
woo-jk <201702042@o.cnu.ac.kr>
2022-12-16 04:24:10 +0900

    fix: InputView 재매칭 입력 함수 개행 처리 추가

--- src/InputView.js
+++ src/InputView.js
@@ -13,7 +13,7 @@ const InputView = {
   },
 
   readRematchingCommand(callback) {
-    MissionUtils.Console.readLine("매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n네 | 아니오\n", callback);
+    MissionUtils.Console.readLine("\n매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n네 | 아니오\n", callback);
   },
 };
 
